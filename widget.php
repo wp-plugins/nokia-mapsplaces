aa7d9f93027c539c596da31cc7fef4115ab88e6d
Skowron <Skowron@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-17 12:36:35 +0000

    fixed version information in plugin directory
    git-svn-id: https://plugins.svn.wordpress.org/nokia-mapsplaces/trunk@491109 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widget.php
+++ widget.php
@@ -1,14 +1,4 @@
 <?php
-/*
-  Plugin Name: Nokia Maps Places
-  Plugin URI: http://wordpress.org/extend/plugins/nokia-mapsplaces/
-  Description: With this plugin you are able to add a places and addresses into a post or a page.
-  Version: 1.0.2
-  Author: Nokia Corporation
-  Author Email: marek.krysiuk@nokia.com
-  License: BSD License
- */
-
 wp_enqueue_script( 'thickbox' );
 wp_enqueue_style( 'thickbox' );
 
