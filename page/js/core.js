b4035f649c502b784c0114da53ba07240cee86b7
radek.adamczyk <radek.adamczyk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-10 14:28:34 +0000

    overriding isPlace function
    git-svn-id: https://plugins.svn.wordpress.org/nokia-mapsplaces/trunk@542420 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- page/js/core.js
+++ page/js/core.js
@@ -72,9 +72,17 @@ jQuery( document ).ready( function(){
         jQuery('#wrapper').removeClass('mapEnabled');
 		showDisplayOptions (widget ? 'nokia.blue.compact' : 'nokia.blue.place');
     }
+    
+    function tplIsPlace(jsonData){
+        if(jsonData && jsonData.location && jsonData.location.address){
+            return true;
+        }
+        
+        return false;
+    }
 
     function hidePlaceWidget(){
-	
+	console.log('hide')
         if(widget){
           setLayout.call(jQuery('#layoutOptions li#layoutCompact')[0]);
         }
@@ -97,6 +105,9 @@ jQuery( document ).ready( function(){
                     iconUrl: 'images/pin.png',
 					tileType: getTileTypeText(map.baseMapType)
                 }
+            },
+            functions: {
+                isPlace: tplIsPlace
             },
             template: template
         });
@@ -251,7 +262,7 @@ jQuery( document ).ready( function(){
 	}
 
 	function setLayout(){
-	
+	console.log('set layout');
 		// Center offsets for templates
 		// To be removed when passing zoomLevel and tileType parameters do not overwrite other module params (like standard offsets)
 		var templateOffsets = {
@@ -310,7 +321,9 @@ jQuery( document ).ready( function(){
 						centerOffset: templateOffsets[activeTemplate],
 						tileType: getTileTypeText(placeWidget.template.getModules("Map")[0].jslMap.baseMapType)
 					}
-				},
+				},functions:{
+	                isPlace: tplIsPlace
+	            },
 				template: activeTemplate
 			});
 			placeWidget.setData (currentPlaceData);
@@ -646,6 +659,9 @@ jQuery( document ).ready( function(){
                 'Map': {
                     iconUrl: 'images/pin.png'
                 }
+            },
+            functions:{
+                isPlace: tplIsPlace
             },
             template: widget ? 'nokia.blue.compact' : 'nokia.blue.place'
         });
